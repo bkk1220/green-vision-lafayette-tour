945d58ae59f4dd2d215120c91ecca8e5ba7b55d3
bkk1220 <121427457+bkk1220@users.noreply.github.com>
2026-05-18 03:05:35 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,308 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-main-entrance",
+      "name": "Main Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.9706765302298095,
+        "pitch": 0.03123576354367863,
+        "fov": 1.7275237718534
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5285408622843413,
+          "pitch": 0.10302054761019797,
+          "rotation": 0,
+          "target": "1-welcome-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-welcome-area",
+      "name": "Welcome Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.36010627214048974,
+        "pitch": -0.02955799539669357,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10309421176804001,
+          "pitch": 0.062147008957735395,
+          "rotation": 0,
+          "target": "2-workstations--materials-flow"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-workstations--materials-flow",
+      "name": "Workstations & Materials Flow",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.9742757160108173,
+        "pitch": -0.012451258054380077,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.280559685939922,
+          "pitch": 0.22448854466583157,
+          "rotation": 0,
+          "target": "3-main-recycling-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-main-recycling-floor",
+      "name": "Main Recycling Floor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09380649669457597,
+          "pitch": 0.08277743275336213,
+          "rotation": 0,
+          "target": "4-activity--common-space"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-activity--common-space",
+      "name": "Activity & Common Space",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.9192163599289493,
+        "pitch": -0.02273828065827388,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.042071390335913,
+          "pitch": 0.0519950010363317,
+          "rotation": 0,
+          "target": "5-activity--common-space"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-activity--common-space",
+      "name": "Activity & Common Space",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.6872743256295397,
+        "pitch": -0.01767836093297781,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7382594205807393,
+          "pitch": 0.03866378210826937,
+          "rotation": 0,
+          "target": "6-lunch-room--shared-space"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-lunch-room--shared-space",
+      "name": "Lunch Room / Shared Space",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.9862015008854605,
+        "pitch": 0.06147340932659162,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.387335330497475,
+          "pitch": 0.007145667040518333,
+          "rotation": 0,
+          "target": "7-commercial-drop-off--loading-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-commercial-drop-off--loading-area",
+      "name": "Commercial Drop-Off & Loading Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.15098315324350153,
+        "pitch": 0,
+        "fov": 1.7275237718534
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Lafayette Virtual Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
